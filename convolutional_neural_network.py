9d177abc289c0cc6f2c99e9936dca9d7c4845dbc
vietanhtran2710 <vietanhtran.uet@gmail.com>
2021-10-07 21:57:34 +0700

    downgrade to tensorflow==2.2.1 to avoid retracing

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -8,7 +8,6 @@ from tensorflow.keras.utils import to_categorical
 
 import random
 
-tf.get_logger().setLevel('ERROR')
 INPUT_SHAPE = 28
 
 def load_data():
@@ -28,15 +27,15 @@ class CNN(object):
     def __init__(self):
         self.x_train, self.y_train, self.x_test, self.y_test = load_data()
         self.rgl_dct = {
-            0: regularizers.L1(1e-4),
-            1: regularizers.L2(1e-4),
-            2: regularizers.L1L2(l1=1e-4, l2=1e-4),
+            0: regularizers.l1(1e-4),
+            1: regularizers.l2(1e-4),
+            2: regularizers.l1_l2(l1=1e-4, l2=1e-4),
             3: None
         }
 
     def choose_data(self):
         small_x, small_y = shuffle(self.x_train, self.y_train)
-        return small_x[:30000], small_y[:30000]
+        return small_x[:5000], small_y[:5000]
 
     def add_dense_layers(self, components, model):
         new_model = model
@@ -477,8 +476,7 @@ class CNN(object):
                         metrics=['accuracy'])
             small_x, small_y = self.choose_data()
             batch_size = components["b"]
-            model.summary()
-            model.fit(small_x, small_y, epochs=5, validation_data=(self.x_test, self.y_test), batch_size=batch_size, verbose = 0)
+            model.fit(small_x, small_y, epochs=2, validation_data=(self.x_test, self.y_test), batch_size=batch_size, verbose=0)
             _, test_acc = model.evaluate(self.x_test,  self.y_test, verbose = 0)
             del model
             return test_acc
